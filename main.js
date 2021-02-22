20d87e8b63bbc2145b9abc9c71b177d500ab1735
cesar juarez <cesar.juarez@codeimmersives.com>
2021-02-22 02:09:50 -0800

    my-code

--- main.js
+++ main.js
@@ -1,6 +1,45 @@
 /******************************
  * YOUR CODE GOES RIGHT HERE! *
  ******************************/
+let character = 22;
+
+
+function isItLong (character){
+  if (character.length > 20 ){
+    console.log ("That's a long string!");
+  }
+}
+
+function isItMedium (character){
+  if (character.length > 10 && character.length  <20){
+    console.log ("That's a regular sized string!")
+  }
+}
+function isItShort (character){
+  if (character.length < 10 ){
+    console.log ("That's a small string!")
+  } 
+  else{
+    console.log ("That's not a small string!")
+  }
+}
+function howLongIsMyString (character){
+if (character.length > 20){
+  console.log ("That's a long string!")
+} else if (character.length > 10 && character.length < 20){
+  console.log ("That's a regular sized string!")
+} else if (character.length < 10 ){
+  console.log ("That's a small string!")
+}
+}
+function instructorHeight (nameOfInstructor){
+if (nameOfInstructor = "colin"){
+  console.log (62)
+}
+if (nameOfInstructor = "Mesuara"){
+  console.log (69)
+}
+}
 
 
 
